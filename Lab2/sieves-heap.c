4a3304922344afb222bf75adf6ff2379a05c9daa
VonGriff <autumndiscord@gmail.com>
2015-09-23 15:37:29 +0200

    sieves-heap funkar

--- Lab2/sieves-heap.c
+++ Lab2/sieves-heap.c
@@ -18,31 +18,47 @@ int print_sieves(int n) { 	//8 382 224 är största talet Johans dator klarar
 	if(n < 2)		//Om mindre än 2, negativt tal eller ej primtal
 		return 0;
 
+	int *sieves = &n;
 	//bool sieves[n];
-	int i, c = 1, max = sqrt(n);
+	int i;
+	int c = 0;
+	int max = sqrt(n);
 
-	for(i = 2; i < n; i++) { //Sätter alla index från 2 - n till 1
-		sieves[i] = TRUE;
-	}
+	sieves = malloc(n*sizeof(int));
+	if(sieves == NULL)
+		printf("ERROR: out of memory. \n");
+	else {
 
-	for(i = 2; i <= max; i++) { // Går igenom alla index från 2 - (roten ur n)
-		if(sieves[i]) {
-			int j;
-			for(j = i*i; j < n; ) { 
-				sieves[j] = FALSE;
-				j += i;
+		/*for(i = 0; i < n; i++) {
+			primes[i] = TRUE
+		}*/
+
+		for(i = 2; i < n; i++) { //Sätter alla index från 2 - n till 1
+			sieves[i] = TRUE;
+		}
+
+		for(i = 2; i <= max; i++) { // Går igenom alla index från 2 - (roten ur n)
+			if(sieves[i]) {
+				int j;
+				for(j = i*i; j < n; ) { 
+					sieves[j] = FALSE;
+					j += i;
+				}
+			}
+		}
+
+		for(i = 2; i < n; i++) {
+			if(sieves[i]) {
+				print_number(i);
+				c++;
+				if(c == COLUMNS) {
+					printf("\n");
+					c = 0;
+				}
 			}
 		}
 	}
-
-	for(i = 2; i < n; i++) {
-		if(sieves[i]) {
-			print_number(i);
-			c++;
-			if(c % COLUMNS) 
-				printf("\n");
-		}
-	}
+	free(sieves);
 	printf("\n");
 	return 0;
 }
